d37459e72acd600ea408d953ef962c0950b72afa
SHI-Yan LEE <69109740+Yozora525@users.noreply.github.com>
2022-05-16 20:20:10 +0800

    20220516-shiyan 1. 新增loading畫面 2. 修改main.scss

---
+++ assets/js/loading.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $.blockUI({
+        message: $("#loading"),
+        css: {
+            "border": "none",
+            "backgroundColor": "rgba(0,0,0,0)",
+            "backgroundImage": "url('LaptopShoppingSiteFront/assets/img/loading.gif')",
+            "backgroundSize": "100%",
+            
+        }
+    }),
+    $.unblockUI();
+}
+);
