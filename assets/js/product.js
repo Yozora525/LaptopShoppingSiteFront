b31795067f8836a96c3a06723f245ccfbb31dc19
SHI-Yan LEE <69109740+Yozora525@users.noreply.github.com>
2022-06-03 22:15:09 +0800

    20220603-shiyan 1. 新增產品頁面js

---
+++ assets/js/product.js
@@ -0,0 +1,28 @@
+$(function() {
+    $('#minus').on('click', function(event){
+        if ((parseInt($('#quan').val())-1) < 1) {
+            $('#quan').val(1);
+        }else{
+            $('#quan').val(parseInt($('#quan').val())-1);
+        }
+    });
+
+    $('#plus').on('click', function(event){
+        if ((parseInt($('#quan').val())+1) >= (parseInt($('#inventory').text()))) {
+            $('#quan').val($('#inventory').text());
+        }else{
+            $('#quan').val(parseInt($('#quan').val())+1);
+        }
+        
+    });
+
+    $('#quan').on('keyup', function(event){
+        if ($('#quan').val().length == 0) {
+            $('#quan').val(1);
+        }
+        if(parseInt($('#quan').val()) > (parseInt($('#inventory').text()))){
+            $('#quan').val($('#inventory').text());
+        }
+        $('#quan').val(parseInt($('#quan').val())); // 避免使用者在數量前打0
+    });
+});
