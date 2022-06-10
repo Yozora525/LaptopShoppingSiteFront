bc72665991a16aeca49cbba500717b2b7ecaae8a
SHI-Yan LEE <69109740+Yozora525@users.noreply.github.com>
2022-06-11 00:17:29 +0800

    20220611-shiyan 1. 新增評論版

--- assets/js/product.js
+++ assets/js/product.js
@@ -39,4 +39,35 @@ $(function() {
         $('#img-show').attr('src', $(this).attr('src'));
         $(this).css('border', '0');
     });
+
+    // $('span[name="new-eval-star"]').on('mouseenter', function(){
+    //     let index = $('span[name="new-eval-star"]').index(this);
+    //     for (let i = 0; i <= index; i++) {
+    //         $($('span[name="new-eval-star"]')[i]).css('color', '#0096C7');
+    //     }
+    // });
+
+    // $('span[name="new-eval-star"]').on('mouseleave', function(){
+    //     let index = $('span[name="new-eval-star"]').index(this);
+    //     let count = $('span[name="new-eval-star"]').length
+    //     for (count; count > index ; count--) {
+    //         $($('span[name="new-eval-star"]')[count]).css('color', '#ddd');
+    //     }
+    // });
+
+    $('span[name="new-eval-star"]').on('click', function(){
+        let index = $('span[name="new-eval-star"]').index(this);
+        let count = $('span[name="new-eval-star"]').length
+        for (let i = 0; i <= index; i++) {
+            $($('span[name="new-eval-star"]')[i]).css('color', '#0096C7');
+            $('input[name="eval-star"]').val($($('span[name="new-eval-star"]')[i]).data('star'));
+        }
+        for (count; count > index ; count--) {
+            $($('span[name="new-eval-star"]')[count]).css('color', '#ddd');
+        }
+
+        
+    });
+
+
 });
